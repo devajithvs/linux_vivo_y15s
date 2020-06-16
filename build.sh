68a14b77219b1e5c3326cc9ff57a4ee37091f309
devajithvs <devajithvs@gmail.com>
2020-06-16 15:06:57 +0000

    first commit

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p out
+export CROSS_COMPILE=$HOME/arm-eabi-4.7/bin/arm-eabi-
+export USE_CCACHE=1
+export ARCH=arm && export SUBARCH=arm
+
+#make clean
+#make mrproper
+
+make -C $PWD O=$PWD/out ARCH=arm goyave3g_defconfig
+make -j$(nproc) -C $PWD O=$PWD/out ARCH=arm
